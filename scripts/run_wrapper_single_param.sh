51ff710b53d48cfc7520c821520d8289090dd3ac
Stan Fris <sfris1@int5.local.snellius.surf.nl>
2025-11-12 16:33:13 +0100

    added files

--- scripts/run_wrapper_single_param.sh
+++ scripts/run_wrapper_single_param.sh
@@ -6,6 +6,6 @@ python scripts/generate_hparams_single_param.py
 BASELINE_JOB_ID=$(sbatch --parsable scripts/test_varying_array_main_single_param.job)
 echo "Submitted baseline job with ID: $BASELINE_JOB_ID"
 
-# --- Submit sweep array with dependency on baseline ---
-SWEEP_JOB_ID=$(sbatch --dependency=afterok:$BASELINE_JOB_ID scripts/test_varying_array_single_param.job)
-echo "Submitted sweep array job with ID: $SWEEP_JOB_ID, dependent on baseline completion"
+# # --- Submit sweep array with dependency on baseline ---
+# SWEEP_JOB_ID=$(sbatch --dependency=afterok:$BASELINE_JOB_ID scripts/test_varying_array_single_param.job)
+# echo "Submitted sweep array job with ID: $SWEEP_JOB_ID, dependent on baseline completion"
