136da3a3128ae7d2cf8c758cf20ac102521707a8
Stan Fris <stanfris2.0@gmail.com>
2026-01-23 09:44:35 +0100

    add files"

--- scripts/generate_hparams_synthetic_feature_separation.py
+++ scripts/generate_hparams_synthetic_feature_separation.py
@@ -6,7 +6,7 @@ hyperparameter_file = 'scripts/hparams_varying_single_experiment.txt'
 hyperparameter_file_main = 'scripts/hparams_varying_single_experiment_main.txt'
 
 parameters = {
-    'experiment': ['Synthetic_Feature_Separation'],
+    'experiment': ['Synthetic_Feature_Separation_2'],
     'data': ['Custom_dataset_deep'],
     'relevance': ['deep'],
     'logging_policy_ranker': ['ordered'],
@@ -21,10 +21,10 @@ parameters = {
     'logging_policy_sampler': ['e_greedy'],
     'save_test_datasets': [True],
     'load_test_datasets': [True],
-    'num_queries': [1000],
+    'num_queries': [10, 100, 1000, 10000],
     'docs_per_group': [10],
     'label_type': ['deep_overlap'],
-    's_doc' : [10]
+    's_doc' : [-0.3, 0.3, 10]
 }
 
 # Helper function to format a line nicely
@@ -71,6 +71,9 @@ with open(hyperparameter_file, "w") as f:
             ".pkl",
         ])
         params['test_click_dataset_name'] = params['test_dataset_name'].replace("dataset", "click_dataset")
+        params['train_clicks'] = params['num_queries']
+        params['val_clicks'] = params['num_queries']
+        params['test_clicks'] = params['num_queries']
         f.write(format_line(params) + "\n")
 num_jobs = sum(1 for _ in itertools.product(*(parameters[k] for k in shift_keys)))
 
